1f652a2a90cce954066041a2ffee473d0dbcfad1
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-20 20:56:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file
 function findMatching(drivers, someStrings) {
-  let matchingDrivers = drivers.filter( name => name.toUpperCase)
+  let matchingDrivers = drivers.filter( name => name.toUpperCase() === someStrings.toUpperCase())
+  return matchingDrivers
 }
